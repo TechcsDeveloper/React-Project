678d966e74baf5802bf61249ee7d4ee1ee1436f5
BABULAL MANDAL <babulalmandal678@gmail.com>
2024-09-08 22:01:19 +0530

    Bharat Clock

---
+++ Bharat-Clock/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ClockHeading from './component/ClockHeading'
+import CurrentTime from './component/CurrentTime'
+
+function App() {
+  
+
+  return (
+    <>
+      <ClockHeading/>
+      <CurrentTime></CurrentTime>
+    </>
+  )
+}
+
+export default App
